65e1dcf55dc8d3af1d85670447e1d5138723d4fa
Manoj Reddy <luckymanoj1116@gmail.com>
2022-12-17 11:06:38 -0500

    final commit

--- CalculatorApplication/Models/Inputs.cs
+++ CalculatorApplication/Models/Inputs.cs
@@ -8,6 +8,7 @@ namespace CalculatorApplication.Models
     public class Inputs
     {
         // taking user input for calculations
+        // user need to enter the values in the list of values so that the desired calculations will be performed
         public List<double> ListofValues { get; set; }
     }
 }
